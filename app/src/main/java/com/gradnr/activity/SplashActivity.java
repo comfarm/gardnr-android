af6ee43c461ef1af5fc4f1d6484f1529a1ff6487
LIM04 <lorenzo.iraj@gmail.com>
2017-07-01 15:07:14 +0800

    Added Login initial page and android material stepper

--- app/src/main/java/com/gradnr/activity/SplashActivity.java
+++ app/src/main/java/com/gradnr/activity/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.gradnr.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        },2000);
+
     }
 }
