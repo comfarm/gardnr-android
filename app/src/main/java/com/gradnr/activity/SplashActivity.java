5cb84b14a68d152c98bd8de809b97d4c7d054933
LIM04 <lorenzo.iraj@gmail.com>
2017-07-01 21:15:16 +0800

    Test

--- app/src/main/java/com/gradnr/activity/SplashActivity.java
+++ app/src/main/java/com/gradnr/activity/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                startActivity(new Intent(SplashActivity.this, SurveyActivity.class));
                 finish();
             }
         },2000);
