4308899a7f9293bf544805ca9bbd250c410ea015
LIM04 <lorenzo.iraj@gmail.com>
2017-07-01 13:54:59 +0800

    Remove ConstraintLayout dependency

---
+++ app/src/main/java/com/gradnr/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.gradnr.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gradnr.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
