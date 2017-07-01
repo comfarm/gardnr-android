8040f101acad97964affcb06ae69d73aed87c1da
LIM04 <lorenzo.iraj@gmail.com>
2017-07-02 02:38:09 +0800

    Retrofit permission denied

---
+++ app/src/main/java/com/gradnr/dto/PlantRecommendationDTO.java
@@ -0,0 +1,13 @@
+package com.gradnr.dao;
+
+import com.google.gson.annotations.Expose;
+
+/**
+ * Created by LIM on 01/07/2017.
+ */
+
+public class PlantRecommendationDTO {
+    
+    @Expose
+
+}
---
+++ app/src/main/java/com/gradnr/service/SurveyService.java
@@ -0,0 +1,8 @@
+package com.gradnr.service;
+
+/**
+ * Created by LIM on 02/07/2017.
+ */
+
+public class SurveyService {
+}
